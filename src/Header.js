f7fd51886711349a3185e1a8da36b8d98114fb6c
Jared Canty <jc@versatol.local>
2020-12-31 16:29:00 -0600

    MERN " I am header"

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css";  // [./] is because this must be a directory
+
+function Header() {
+    return (
+        <div className='header'>
+         <h2> I am header</h2>
+            
+        </div>
+    )
+}
+
+export default Header
